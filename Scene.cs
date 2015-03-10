e00f3671e9842f18e62855690dd4b84f280f198d
irakaira <ksp95post@gmail.com>
2015-03-10 17:00:34 +0200

    Update Scene.cs

--- Scene.cs
+++ Scene.cs
@@ -1,1 +1,32 @@
-# Project_IPZ
+class Account
+{
+  private string name;
+  private bool sex;/0-male 1-female
+  protected int lvl;
+  Account(string name,bool sex)
+  {
+    this.lvl=0;
+    this.name=name;
+    this.sex=sex;
+  }
+  string getName()
+  {
+    return this.name;
+  }
+  bool getSex()
+  {
+    return this.sex;
+  }
+  int getLvl()
+  {
+    return this.lvl;
+  }
+  void setName(string name)
+  {
+    this.name=name;
+  }
+  void setLvl()
+  {
+    lvl++;
+  }
+}
